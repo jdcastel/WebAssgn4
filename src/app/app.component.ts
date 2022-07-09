8f045662b2b42db0f4d84a12cb56ae9c4828a321
Juan David Castelblanco <84249113+jdcastel@users.noreply.github.com>
2022-07-08 22:43:18 -0400

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+/*********************************************************************************
+* WEB422 – Assignment 04
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
+* assignment has been copied manually or electronically from any other source (including web sites) or 
+* distributed to other students.
+* 
+* Name: Juan David Rodriguez Castelblanco Student ID: 147891204 Date: 08/07/2022
+*
+********************************************************************************/
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'web422-a4';
+}
